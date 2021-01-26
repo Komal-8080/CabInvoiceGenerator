5c5e9c9fe4c268a7ec26373df2993de8d7a569b2
Komal-8080 <shindeg163@gmail.com>
2021-01-27 00:34:15 +0530

    Added: If Given Distance Or Time Is Less Then Should Return Minimum Fare and Should Pass Test Case

--- lib/src/main/java/InvoiceService/InvoiceGenerator.java
+++ lib/src/main/java/InvoiceService/InvoiceGenerator.java
@@ -3,10 +3,11 @@ package InvoiceService;
 public class InvoiceGenerator {
 
 	public static final double MINIMUM_COST_PER_KILOMETER = 10.0;
-	   public static final int COST_PER_TIME = 1;
+	public static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
 
-	   public double calculateFare(double distance, int time)
-	   {
-	      return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-	   }
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+		return Math.max(totalFare, MINIMUM_FARE);
+	}
 }
